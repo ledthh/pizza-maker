772c9dd51c0f0598cce46775c279732ef746bc70
Ле Динь Хией <33816369+ledthh@users.noreply.github.com>
2021-05-06 18:00:00 +0300

    Added images for sauce, dough, pizza size, bg Updated home page

---
+++ src/components/PizzaMaker.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12" class="text-center white--text">
+        <h1>Заказ пиццы</h1>
+      </v-col>
+      <v-col cols="12">
+        <v-card min-height="400" rounded="xl" class="pa-2">
+          
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'PizzaMaker',
+    data() {
+      return {}
+    }
+  }
+</script>
