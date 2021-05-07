38134bc0d91c96bd67e67d446e881b0fa5a851a9
Ле Динь Хией <33816369+ledthh@users.noreply.github.com>
2021-05-07 18:53:53 +0300

    Added ingredients in data.js

--- src/data.js
+++ src/data.js
@@ -58,5 +58,159 @@ export default {
       src: require('@/assets/img/sauce/ranch.png')
     },
   ],
-  ingredients: [],
+  ingredients: {
+    meat: [
+      {
+        id: 'bacon',
+        name: 'Бекон',
+        price: 40,
+        src: require('@/assets/img/ingredients/meat/bacon.png'),
+        count: 0
+      },
+      {
+        id: 'beef',
+        name: 'Говядина (фарш)',
+        price: 40,
+        src: require('@/assets/img/ingredients/meat/beef.png'),
+        count: 0
+      },
+      {
+        id: 'chicken',
+        name: 'Курица гриль',
+        price: 40,
+        src: require('@/assets/img/ingredients/meat/chicken.png'),
+        count: 0
+      },
+      {
+        id: 'ham',
+        name: 'Ветчина',
+        price: 40,
+        src: require('@/assets/img/ingredients/meat/ham.jpg'),
+        count: 0
+      },
+      {
+        id: 'pork',
+        name: 'Свинина (фарш)',
+        price: 40,
+        src: require('@/assets/img/ingredients/meat/pork.jpg'),
+        count: 0
+      },
+      {
+        id: 'salami',
+        name: 'Салями',
+        price: 40,
+        src: require('@/assets/img/ingredients/meat/salami.png'),
+        count: 0
+      },
+    ],
+    veg: [
+      {
+        id: 'black_olives',
+        name: 'Маслины',
+        price: 30,
+        src: require('@/assets/img/ingredients/муп/black_olives.jpg'),
+        count: 0
+      },
+      {
+        id: 'champignon',
+        name: 'Шампиньоны',
+        price: 30,
+        src: require('@/assets/img/ingredients/муп/champignon.jpg'),
+        count: 0
+      },
+      {
+        id: 'corn',
+        name: 'Кукуруза',
+        price: 30,
+        src: require('@/assets/img/ingredients/муп/corn.jpg'),
+        count: 0
+      },
+      {
+        id: 'cucumber',
+        name: 'Огурец маринованый',
+        price: 30,
+        src: require('@/assets/img/ingredients/муп/cucumber.jpg'),
+        count: 0
+      },
+      {
+        id: 'olives',
+        name: 'Оливки',
+        price: 30,
+        src: require('@/assets/img/ingredients/муп/olives.jpg'),
+        count: 0
+      },
+      {
+        id: 'onion',
+        name: 'Лук',
+        price: 30,
+        src: require('@/assets/img/ingredients/муп/onion.jpg'),
+        count: 0
+      },
+      {
+        id: 'parsley',
+        name: 'Петрушка',
+        price: 30,
+        src: require('@/assets/img/ingredients/муп/parsley.jpg'),
+        count: 0
+      },
+      {
+        id: 'pepper',
+        name: 'Перец болгарский',
+        price: 30,
+        src: require('@/assets/img/ingredients/муп/pepper.jpg'),
+        count: 0
+      },
+      {
+        id: 'semsame',
+        name: 'Кунжут',
+        price: 30,
+        src: require('@/assets/img/ingredients/муп/semsame.jpg'),
+        count: 0
+      },
+      {
+        id: 'tomatoe',
+        name: 'Помидор',
+        price: 30,
+        src: require('@/assets/img/ingredients/муп/tomatoe.jpg'),
+        count: 0
+      },
+    ],
+    cheese: [
+      {
+        id: 'cheddar',
+        name: 'Чеддер',
+        price: 35,
+        src: require('@/assets/img/ingredients/cheese/cheddar.jpg'),
+        count: 0
+      },
+      {
+        id: 'dor_blue',
+        name: 'Дор блю',
+        price: 35,
+        src: require('@/assets/img/ingredients/cheese/dor_blue.jpg'),
+        count: 0
+      },
+      {
+        id: 'feta',
+        name: 'Фета',
+        price: 35,
+        src: require('@/assets/img/ingredients/cheese/feta.jpg'),
+        count: 0
+      },
+      {
+        id: 'parmesan',
+        name: 'Пармезан',
+        price: 35,
+        src: require('@/assets/img/ingredients/cheese/parmesan.jpg'),
+        count: 0
+      },
+      {
+        id: 'philadelphia',
+        name: 'Филадельфия',
+        price: 35,
+        src: require('@/assets/img/ingredients/cheese/philadelphia.jpg'),
+        count: 0
+      },
+    ],
+  },
 }
