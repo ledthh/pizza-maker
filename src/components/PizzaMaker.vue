8fb1bd97d32e8ce499393b5379a0317dd1edf04f
Ле Динь Хией <33816369+ledthh@users.noreply.github.com>
2021-05-07 12:22:59 +0300

    Added dough, size & sauce selector Added data.js file with info

--- src/components/PizzaMaker.vue
+++ src/components/PizzaMaker.vue
@@ -5,8 +5,96 @@
         <h1>Заказ пиццы</h1>
       </v-col>
       <v-col cols="12">
-        <v-card min-height="400" rounded="xl" class="pa-2">
+        <v-card rounded="xl" class="pa-2">
+          <v-row>
+            <v-col cols="12" sm="6">
+              <v-img src="@/assets/img/pizza_example.jpg" aspect-ratio="1" />
+            </v-col>
+            <v-col cols="12" sm="6">
+              <v-row>
+                <v-col cols="12" class="text-center">
+                  <h2>Конструктор пиццы</h2>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col cols="4">
+                  <h3>Тесто</h3>
+                  <div>{{ selectedDough }}</div>
+                </v-col>
+                <v-col cols="8">
+                  <v-row>
+                    <v-col
+                      cols="4"
+                      v-for="item in dough"
+                      :key="item.id"
+                    >
+                      <v-img
+                        :style="{ opacity: doughOpacity(item.id) }"
+                        :id="item.id"
+                        :src="item.src"
+                        @click="changeDough(item.id)"
+                      />
+                    </v-col>
+                  </v-row>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col cols="4">
+                  <h3>Размер</h3>
+                  <div>{{ selectedSize }} см</div>
+                </v-col>
+                <v-col cols="8">
+                  <v-row>
+                    <v-col
+                      cols="4"
+                      v-for="size in sizes"
+                      :key="size.id"
+                    >
+                      <img
+                        height="50"
+                        :style="{ opacity: sizeOpacity(size.id) }"
+                        :id="size.id"
+                        :src="size.src"
+                        @click="changeSize(size.id)"
+                      />
+                    </v-col>
+                  </v-row>
+                    
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col cols="4">
+                  <h3>Соус</h3>
+                  <div>{{ selectedSauce }}</div>
+                </v-col>
+                <v-col cols="8">
+                  <v-row>
+                    <v-col
+                      cols="3"
+                      v-for="sauce in sauces"
+                      :key="sauce.id"
+                    >
+                      <img
+                        height="50"
+                        :style="{ opacity: sauceOpacity(sauce.id) }"
+                        :id="sauce.id"
+                        :src="sauce.src"
+                        @click="changeSauce(sauce.id)"
+                      />
+                    </v-col>
+                  </v-row>
+                </v-col>
+              </v-row>
+            </v-col>
+          </v-row>
+
+          <v-row>
+            <!-- Ингредиенты -->
+          </v-row>
           
+          <v-row>
+            <!-- Счётчик - сброс - заказать -->
+          </v-row>
         </v-card>
       </v-col>
     </v-row>
@@ -14,10 +102,75 @@
 </template>
 
 <script>
-  export default {
-    name: 'PizzaMaker',
-    data() {
-      return {}
+import source from '@/data.js';
+
+export default {
+  name: 'PizzaMaker',
+  data() {
+    return {
+      currentDough: 'americano',
+      currentSize: 'size-small',
+      currentSauce: 'garlic',
+      sauces: source.sauces,
+      dough: source.dough,
+      sizes: source.sizes,
+      ingredients: source.ingredients
     }
+  },
+  computed: {
+    selectedDough() {
+      let selected = '';
+
+      this.dough.forEach((item) => {
+        if (item.id === this.currentDough) {
+          selected = item.name;
+        }
+      });
+
+      return selected;
+    },
+    selectedSize() {
+      let selected = '';
+
+      this.sizes.forEach((item) => {
+        if (item.id === this.currentSize) {
+          selected = item.size;
+        }
+      });
+
+      return selected;
+    },
+    selectedSauce() {
+      let selected = '';
+
+      this.sauces.forEach((item) => {
+        if (item.id === this.currentSauce) {
+          selected = item.name;
+        }
+      });
+
+      return selected;
+    }
+  },
+  methods: {
+    doughOpacity(item) {
+      return this.currentDough === item ? 1 : 0.4;
+    },
+    changeDough(item) {
+      this.currentDough = item;
+    },
+    sizeOpacity(item) {
+      return this.currentSize === item ? 1 : 0.4;
+    },
+    changeSize(item) {
+      this.currentSize = item;
+    },
+    sauceOpacity(item) {
+      return this.currentSauce === item ? 1 : 0.4;
+    },
+    changeSauce(item) {
+      this.currentSauce = item;
+    },
   }
+}
 </script>
---
+++ src/data.js
@@ -0,0 +1,62 @@
+export default {
+  dough: [
+    {
+      id: 'americano',
+      name: 'Американо',
+      src: require('@/assets/img/dough/americano.png')
+    },
+    {
+      id: 'italiano',
+      name: 'Италиано',
+      src: require('@/assets/img/dough/italiano.png')
+    },
+    {
+      id: 'hotdog',
+      name: 'Хот-дог',
+      src: require('@/assets/img/dough/hotdog.png')
+    }
+  ],
+  sizes: [
+    {
+      id: 'size-small',
+      src: require('@/assets/img/size/small.png'),
+      size: 20,
+      price: 50
+    },
+    {
+      id: 'size-medium',
+      src: require('@/assets/img/size/medium.png'),
+      size: 30,
+      price: 100
+    },
+    {
+      id: 'size-large',
+      src: require('@/assets/img/size/large.png'),
+      size: 40,
+      price: 150
+    }
+  ],
+  sauces: [
+    {
+      id: 'garlic',
+      name: 'Гарлик',
+      src: require('@/assets/img/sauce/garlic.png')
+    },
+    {
+      id: 'salsa',
+      name: 'Сальса',
+      src: require('@/assets/img/sauce/salsa.png')
+    },
+    {
+      id: 'bbq',
+      name: 'Барбекю',
+      src: require('@/assets/img/sauce/bbq.png')
+    },
+    {
+      id: 'ranch',
+      name: 'Ранч',
+      src: require('@/assets/img/sauce/ranch.png')
+    },
+  ],
+  ingredients: [],
+}
